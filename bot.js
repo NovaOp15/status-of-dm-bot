ddf25d0205ac515031471aa8ee82dd5af0fa3275
bhupenderrao369 <63923037+bhupenderrao369@users.noreply.github.com>
2020-07-14 01:28:02 +0530

    Delete bot.js

--- bot.js
+++
@@ -1,6 +0,0 @@
-bot.on("warn", console.warn);
-bot.on("error", console.error);
-bot.on("ready", () =>{
-  console.log(${bot.user.tag} has been successfully turned on!)
- bot.user.setActivity(status ,{type:"WATCHING"})
-});
